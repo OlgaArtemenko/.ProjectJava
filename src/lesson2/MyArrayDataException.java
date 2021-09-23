9d3f0364b0131c8f856b036054699bd4ba710cd7
Artemenko <olga_silyutina@mail.ru>
2021-09-23 13:27:24 +0300

    DZ2

---
+++ src/lesson2/MyArrayDataException.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class MyArrayDataException extends NumberFormatException{
+    public void MyArrayDataException(){
+
+        System.out.print("MyArrayDataException: Некорректный элемент массива");
+
+    }
+}
---
+++ src/lesson2/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class MyArraySizeException extends Exception {
+    public void MyArraySizeException(String message) {
+
+        System.out.println("Введен массив некорректного размера!");
+    }
+
+}
+
